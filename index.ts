064991953953a7d42f34eb2ccdb022612e803cc7
hassanrammal <130249109+elitehassan@users.noreply.github.com>
2024-03-31 18:52:46 -0400

    hassan_changes_to_user_variable_31032024

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
 import { Bot } from "grammy";
 
 const bot = new Bot("6982927940:AAHxd-jtUvEeWrLyWIeOKytcmlEaym_TuZc");
-let user: { 
-  username: string;
-  userId: number;
-  }
+
+let user: {};
 
 bot.command("start", async (ctx) => {
   await ctx.reply(
@@ -23,13 +21,12 @@ bot.use( async (ctx) => {
   if (ctx.from?.id!=undefined){
   userId = ctx.from.id;
   }
-
- const user = {
-  username: username,
-  userId: userId,
- };
- 
+   user = {
+    username: username,
+    userId: userId
+  }
 });
+
 
 bot.command("getId", async (ctx) => {
   await ctx.reply(user.userId.toString());
