149da05d40266691971081e1b058578c0517ba27
elitehassan <hasanrammal7@gmail.com>
2024-04-18 13:45:46 -0400

    hassan_added_types_mysql_18042024
    connected the bot to the database and added code so that the pulled userID and username can be sent to the database

--- index.ts
+++ index.ts
@@ -1,7 +1,19 @@
 import { Bot } from "grammy";
 import { ethers } from "ethers";
+import { createConnection, Connection } from "mysql";
+
 
 const bot = new Bot("6982927940:AAHxd-jtUvEeWrLyWIeOKytcmlEaym_TuZc");
+
+let connection: Connection;
+async function connectToDatabase(){
+  connection = await createConnection({
+    host: "172.22.48.66",
+    user: "root",
+    password: "My$QLpa$$w0rd",
+    database: "botinfo"
+  });
+}
 
 let user: {};
 
@@ -14,6 +26,8 @@ bot.command("start", async (ctx) => {
 bot.use( async (ctx) => {
   let username;
   let userId;
+  const firstName = ctx.from?.first_name;
+  const lastName = ctx.from?.last_name;
 
   if (ctx.from?.username!=undefined){
   username=ctx.from.username;
@@ -21,11 +35,17 @@ bot.use( async (ctx) => {
   if (ctx.from?.id!=undefined){
   userId = ctx.from.id;
   }
-   user = {
-    username: username,
-    userId: userId
+  if (username && userId && firstName && lastName){
+    const query = "INSERT INTO info (userId, username, first_name, last_name, last_seen) VALUES (?, ?, ?, ?, NOW())";
+    connection.query(query, [userId, username, firstName, lastName], (error, results, fields) => {
+      if (error) {
+        console.error("Error inserting user: ", error); 
+        return; 
+      }
+      console.log("user inserted successfuly ");
+     });
+   }
   };
-}
 });
 
 // bot.command("getId", async (ctx) => {
