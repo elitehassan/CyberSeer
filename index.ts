4160299362372598da84df060acbc62adacba575
elitehassan <hasanrammal7@gmail.com>
2024-05-27 12:21:31 -0400

    hassan_2305202024
    deleted the tokenQuery

--- index.ts
+++ index.ts
@@ -23,38 +23,38 @@ async function connectToDatabase(ctx: Context) {
 let userId=0;
 
 bot.command("start", async (ctx) => {
-    await connectToDatabase(ctx);
+    // await connectToDatabase(ctx);
     await ctx.reply(
         "My name is the CyberSeer, and I will be assisting you with your crypto magik."
     );
-    let username:string="";
-    const firstName = ctx.from?.first_name;
-    const lastName = ctx.from?.last_name;
-    if (ctx.from?.username != undefined) {
-        username = ctx.from.username;
-    }
-    if (ctx.from?.id != undefined) {
-        userId = ctx.from.id;
-    }
-    ctx.reply("making a check")
-    if (username && userId && firstName && lastName) {
-        ctx.reply("sending message");
-        const query = "INSERT INTO info (user_Id, username, first_name, last_name, last_seen) VALUES (?, ?, ?, ?, NOW()) on duplicate key update last_seen=NOW();";
-        const userIdTokenQuery = "INSERT IGNORE INTO tokens (user_Id) VALUES (?);";
-        connection.query(query, [userId, username, firstName, lastName], (error, results, fields) => {
-            if (error) {
-                ctx.reply("Error inserting user. ");
-                return;
-            }
-            connection.query(userIdTokenQuery, [userId], (tokenError) => {
-                if (tokenError) {
-                    ctx.reply("Error inserting user to the token table.");
-                    return;
-                }
-                ctx.reply("user inserted successfuly ");
-            });
-        });
-    }
+    // let username:string="";
+    // const firstName = ctx.from?.first_name;
+    // const lastName = ctx.from?.last_name;
+    // if (ctx.from?.username != undefined) {
+    //     username = ctx.from.username;
+    // }
+    // if (ctx.from?.id != undefined) {
+    //     userId = ctx.from.id;
+    // }
+    // ctx.reply("making a check")
+    // if (username && userId && firstName && lastName) {
+    //     ctx.reply("sending message");
+    //     const query = "INSERT INTO info (user_Id, username, first_name, last_name, last_seen) VALUES (?, ?, ?, ?, NOW()) on duplicate key update last_seen=NOW();";
+    //     const userIdTokenQuery = "INSERT IGNORE INTO tokens (user_Id) VALUES (?);";
+    //     connection.query(query, [userId, username, firstName, lastName], (error, results, fields) => {
+    //         if (error) {
+    //             ctx.reply("Error inserting user. ");
+    //             return;
+    //         }
+    //         connection.query(userIdTokenQuery, [userId], (tokenError) => {
+    //             if (tokenError) {
+    //                 ctx.reply("Error inserting user to the token table.");
+    //                 return;
+    //             }
+    //             ctx.reply("user inserted successfuly ");
+    //         });
+    //     });
+    // }
 });
 
 bot.command("getblocknumber", async (ctx) => {
@@ -79,32 +79,47 @@ bot.command("pullinfo", async (ctx) => {
             reply_parameters: { message_id: ctx.msg.message_id }
         });
         
+
+
+
     } catch (error) {
         console.error("Error fetching token information", error)
         await ctx.reply("Error fetching token information. Please notify Rythm.")
     }
-    const tokenQuery = `DECLARE count int;
-    DECLARE origin VARCHAR(42);
-    DECLARE dest VARCHAR(42);
-    SET count = 4;
-    WHILE count >= 1 DO
-    SET origin=concat('token',count);
-    SET dest=concat('token',count+1);    
-    UPDATE botinfo.tokens
-    set dest = (SELECT origin FROM botinfo.tokens WHERE user_id=?)
-    WHERE user_id=?;
-    set count =count-1;
-    END WHILE; 
-    UPDATE tokens SET token1=? WHERE user_id=?;`;
-    connection.query(tokenQuery, [userId, userId, tokenAddress, userId], (error, results, fields) =>{
-    if (error) {
-        ctx.reply("Error inserting token into the table.");
-        return;
-    }
-    ctx.reply("token inserted")
-    });
+    // const tokenQuery = `DECLARE count int;
+    // DECLARE origin VARCHAR(42);
+    // DECLARE dest VARCHAR(42);
+    // SET count = 4;
+    // WHILE count >= 1 DO
+    // SET origin=concat('token',count);
+    // SET dest=concat('token',count+1);    
+    // UPDATE botinfo.tokens
+    // set dest = (SELECT origin FROM botinfo.tokens WHERE user_id=?)
+    // WHERE user_id=?;
+    // set count =count-1;
+    // END WHILE; 
+    // UPDATE tokens SET token1=? WHERE user_id=?;`;
+    // connection.query(tokenQuery, [userId, userId, tokenAddress, userId], (error, results, fields) =>{
+    // if (error) {
+    //     ctx.reply("Error inserting token into the table.");
+    //     return;
+    // }
+    // ctx.reply("token inserted")
+    // });
 });
 
+
+function formatNumber(value:number): string {
+    if (value >= 1e9){
+        return (value / 1e9).toFixed(2).toString() + 'b';
+    } else if (value >= 1e6) {
+        return (value / 1e6).toFixed(2) + 'm';
+    } else if (value >= 1e3){
+        return (value / 1e3).toFixed(2) + 'k';
+    }else {
+        return value.toString();   
+    }
+}
 
 async function getTokenDetails(tokenAddress: string) {
     const contract = new ethers.Contract(tokenAddress, tokenAbi, ankrProvider);
@@ -119,14 +134,14 @@ async function getTokenDetails(tokenAddress: string) {
     // to make this more complicated
     return {
         tokenName: tokenName,
-        tokenSupply: ethers.formatUnits(totalSupply, 18).toString(),
+        tokenSupply: totalSupply,
         // tokenMaxBuy: tokenMaxBuy.toString(),
     }
 
 }
 function formatTokenDetails(tokenDetails: any): string {
     return `Token Name: ${tokenDetails.tokenName}
-  Total Supply: ${tokenDetails.tokenSupply}
+  Total Supply: ${formatNumber(tokenDetails.tokenSupply)}
   Max Buy: ${tokenDetails.tokenMaxBuy}
   `
 }
